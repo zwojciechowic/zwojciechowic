f790c6b5cbab44958d15c6a66cc44d13f23082a0
“tmczs” <tmczs@proton.me>
2025-07-22 18:54:59 +0200

    modyfikacja settings env

--- zwojciechowic/settings.py
+++ zwojciechowic/settings.py
@@ -167,3 +167,18 @@ if not DEBUG:
 ADMIN_SITE_HEADER = "🏡 Hodowla z Wojciechowic"
 ADMIN_SITE_TITLE = "Hodowla Admin"
 ADMIN_INDEX_TITLE = "Panel Zarządzania"
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'ERROR',
+    },
+}
