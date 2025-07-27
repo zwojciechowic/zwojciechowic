6213127cee7e81b9cbef8a2c786712305a7270c9
“tmczs” <tmczs@proton.me>
2025-07-27 10:22:17 +0200

    side navbar admin

--- zwojciechowic/urls.py
+++ zwojciechowic/urls.py
@@ -5,11 +5,11 @@ from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
 ]
 
 urlpatterns += i18n_patterns(
-    path('admin/', admin.site.urls),  # ← PRZENIEŚ TUTAJ
     path('', include('main.urls')),
     prefix_default_language=True
 )
