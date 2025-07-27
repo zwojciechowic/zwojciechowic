9a21d950098c5384044588f66f42463d24269caa
“tmczs” <tmczs@proton.me>
2025-07-27 08:49:05 +0200

    side navbar admin

--- zwojciechowic/settings.py
+++ zwojciechowic/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
+    'main.apps.MainConfig',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
