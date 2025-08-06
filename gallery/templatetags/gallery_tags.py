c83d5fb5cbecab1a683f06a62a8be31403a94af7
“tmczs” <tmczs@proton.me>
2025-08-06 17:50:02 +0200

    poprawa zdjecwyswietlanych

--- gallery/templatetags/gallery_tags.py
+++ gallery/templatetags/gallery_tags.py
@@ -3,14 +3,25 @@ from ..models import Gallery
 
 register = template.Library()
 
+# Zmieniamy definicję tagu, dodając drugi, opcjonalny argument
 @register.inclusion_tag('gallery/gallery.html')
-def show_gallery(gallery_id):
+def show_gallery(gallery_id, extra_classes=""):
+    """
+    Tag szablonu do wyświetlania galerii zdjęć.
+    
+    Użycie:
+    {% show_gallery gallery.id %}
+    {% show_gallery gallery.id "moja-klasa-css" %}
+    """
     try:
-        # Użycie prefetch_related jest dobrą praktyką dla optymalizacji zapytań
         gallery = Gallery.objects.prefetch_related('photos').get(id=gallery_id)
         photos = gallery.photos.all()
     except Gallery.DoesNotExist:
-        # Eleganckie obsłużenie przypadku, gdy galeria nie istnieje
+        gallery = None
         photos = []
         
-    return {'photos': photos}
+    return {
+        'gallery': gallery,
+        'photos': photos,
+        'extra_classes': extra_classes, # Przekazujemy klasy do szablonu
+    }
