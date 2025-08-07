0f521d55edae47c7c6f73a882d05379c8ffdeeab
“tmczs” <tmczs@proton.me>
2025-08-07 19:11:09 +0200

    uklad detal-psa | glowne zdjecie naprawa

--- main/admin.py
+++ main/admin.py
@@ -21,6 +21,7 @@ class DogAdminForm(forms.ModelForm):
     
     class Media:
         js = ('js/admin_image_preview.js',)
+        css = {'all': ('css/admin/admin_custom.css',)}
 
 class PuppyAdminForm(forms.ModelForm):
     class Meta:
