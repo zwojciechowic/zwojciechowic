a1bd641b386931bbfcec48724cf98191c59460bd
“tmczs” <tmczs@proton.me>
2025-08-07 23:00:53 +0200

    blog zdjecia

--- main/models.py
+++ main/models.py
@@ -9,7 +9,14 @@ class BlogPost(models.Model):
     slug = models.SlugField(unique=True, verbose_name='URL (slug)')
     content = models.TextField(verbose_name='Treść')
     excerpt = models.TextField(max_length=300, blank=True, verbose_name='Krótki opis')
-    featured_image = models.ImageField(upload_to='blog_images/', blank=True, null=True, verbose_name='Zdjęcie główne')
+    photo_gallery = models.ForeignKey(
+        'gallery.Gallery', 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True, 
+        verbose_name='Galeria zdjęć',
+        related_name='blog_posts'
+    )
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Autor')
     created_at = models.DateTimeField(default=timezone.now, verbose_name='Data utworzenia')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Data aktualizacji')
@@ -22,7 +29,13 @@ class BlogPost(models.Model):
     
     def __str__(self):
         return self.title
-
+    
+    @property
+    def main_photo(self):
+        """Pierwsze zdjęcie z galerii jako główne"""
+        if self.photo_gallery:
+            return self.photo_gallery.photos.first()
+        return None
 class Dog(models.Model):
     name = models.CharField(max_length=100, verbose_name='Imię')
     breed = models.CharField(max_length=100, verbose_name='Rasa')
@@ -102,6 +115,7 @@ class Puppy(models.Model):
         if self.photo_gallery:
             return self.photo_gallery.photos.first()
         return None
+
 class Reservation(models.Model):
     puppy = models.ForeignKey(Puppy, on_delete=models.CASCADE, verbose_name='Szczeniak')
     customer_name = models.CharField(max_length=100, verbose_name='Imię i nazwisko')
