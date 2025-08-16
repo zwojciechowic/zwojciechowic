025fc3926b0e13938a9d5fe057eb7bd07d03206c
“tmczs” <tmczs@proton.me>
2025-08-16 18:48:14 +0200

    proba wyslania mesages reservations trans

--- main/forms.py
+++ main/forms.py
@@ -29,25 +29,80 @@ class PuppyReservationForm(forms.ModelForm):
             'customer_phone': _('Telefon'),
         }
 
-# Tymczasowo BEZ 'message' w fields - dodaj po makemigrations
 class ReservationForm(forms.ModelForm):
+    message = forms.CharField(
+        required=False,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control', 
+            'rows': 4,
+            'placeholder': _('Dodatkowa wiadomość (opcjonalnie)')
+        }),
+        label=_('Wiadomość')
+    )
+    
     class Meta:
         model = Reservation
-        fields = ['puppy', 'customer_name', 'customer_email', 'customer_phone']  # <- usunieto 'message'
+        fields = ['puppy', 'customer_name', 'customer_email', 'customer_phone']
         widgets = {
             'puppy': forms.Select(attrs={'class': 'form-control'}),
-            'customer_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'customer_email': forms.EmailInput(attrs={'class': 'form-control'}),
-            'customer_phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'customer_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': _('Wpisz swoje imię i nazwisko')
+            }),
+            'customer_email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': _('Wpisz swój adres e-mail')
+            }),
+            'customer_phone': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': _('Wpisz swój numer telefonu')
+            }),
+        }
+        labels = {
+            'puppy': _('Szczeniak'),
+            'customer_name': _('Imię i nazwisko'),
+            'customer_email': _('E-mail'),
+            'customer_phone': _('Telefon'),
         }
 
-# Tymczasowo BEZ 'subject', 'message' w fields - dodaj po makemigrations  
 class ContactForm(forms.ModelForm):
+    subject = forms.CharField(
+        max_length=200,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': _('Wpisz temat wiadomości')
+        }),
+        label=_('Temat')
+    )
+    
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'rows': 5,
+            'placeholder': _('Wpisz swoją wiadomość')
+        }),
+        label=_('Wiadomość')
+    )
+    
     class Meta:
         model = ContactMessage
-        fields = ['name', 'email', 'phone']  # <- usunieto 'subject', 'message'
+        fields = ['name', 'email', 'phone']
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
-            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': _('Wpisz swoje imię i nazwisko')
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': _('Wpisz swój adres email')
+            }),
+            'phone': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': _('Wpisz swój numer telefonu')
+            }),
+        }
+        labels = {
+            'name': _('Imię i nazwisko'),
+            'email': _('E-mail'),
+            'phone': _('Telefon'),
         }
