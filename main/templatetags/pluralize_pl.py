bff26c26d7034b84503bd5f203104ea4c6f0fe45
“tmczs” <tmczs@proton.me>
2025-08-16 17:53:09 +0200

    other translate issues

---
+++ main/templatetags/pluralize_pl.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def plural_suffix_pl(value):
+    value = int(value)
+    if value == 1:
+        return ''
+    if 2 <= value % 10 <= 4 and (value % 100 < 10 or value % 100 >= 20):
+        return 'y'  # 2,3,4,22,23,24,32,33,34 itd. (ale nie 12,13,14)
+    return 'ów'  # 0,5,6,7,8,9,10-21,25-31 itd.
